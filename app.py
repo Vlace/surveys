cad5a8c4550779fa7b127715f0c48a319eced7a8
vlace <apandreson@yahoo.com>
2022-02-03 15:46:26 -0500

    survey session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from surveys import surveys
 
 from flask_debugtoolbar import DebugToolbarExtension
@@ -8,6 +8,7 @@ app.config['SECRET_KEY'] = "oh-so-secret"
 
 debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
 responses = []
 
 
@@ -16,16 +17,25 @@ def home():
     satisfaction_survey = surveys["satisfaction"]
     return render_template('questions.html', satisfaction_survey=satisfaction_survey)
 
+
+
+@app.route("/reset_responses", methods=["POST"])
+def reset():
+    session["responses"] = []
+    return redirect("/questions/0")
+
 @app.route("/questions/<int:quest_num>")
 def questions(quest_num):
+    
+    responses= session["responses"]
     satisfaction_survey = surveys["satisfaction"].questions
     num = quest_num
     satisfaction_choices = satisfaction_survey[num].choices
     
-    if(len(responses) == len(satisfaction_survey)):
+    if(len(session["responses"]) == len(satisfaction_survey)):
         return redirect("/survey_complete")
     
-    if (len(responses) != num):
+    if (len(session["responses"]) != num):
         flash("You shall not pass!")
         return redirect (f"/questions/{len(responses)}")
     else:
@@ -34,7 +44,10 @@ def questions(quest_num):
 @app.route("/answers", methods=["POST"])
 def answers():
     answer = request.form["answer"]
-    responses.append(answer)
+    answer_list = session["responses"]
+    answer_list.append(answer)
+    session["responses"] = answer_list
+    responses = session["responses"]
     satisfaction_survey = surveys["satisfaction"].questions
 
     if len(responses) == len(satisfaction_survey):
